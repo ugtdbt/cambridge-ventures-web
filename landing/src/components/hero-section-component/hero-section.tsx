21df780f5bd0925294434e9584912c13d81ef9b5
ramesh-madara <rameshmadara@gmail.com>
2024-11-08 13:51:50 +0530

    hero section 1 mobile

--- landing/src/components/hero-section-component/hero-section.tsx
+++ landing/src/components/hero-section-component/hero-section.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import "./hero-section.modules.css";
 import headerImage from "../../assets/images/header_img.png";
-import logo from "../../assets/images/logo.svg";
+import logo from "../../assets/images/logo_cv.png";
 
 interface HeroSectionProps {}
 
 const HeroSection: React.FC<HeroSectionProps> = () => {
   return (
-    <div className="header-outer">
-      <div className="header-image"></div>
-      <div className="header-content">
-        <div className="header-logo">
-          <img src={logo} alt="" />
-        </div>
-        <div className="header-title-outer">
-          <h1 className="header-title">
-            CAMBRIDGE <br /> VENTURES
-          </h1>
-          <p className="header-subtext">
-            We structure and execute real estate development projects in a
-            responsible and rewarding manner based on careful asset selection,
-            optimal value addition, innovative financing structures and targeted
-            marketing and sales strategies.
-          </p>
+    <div>
+      <div className="header-outer">
+        <div className="header-image"></div>
+        <div className="header-content">
+          <div className="header-logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="header-title-outer">
+            <h1 className="header-title">
+              CAMBRIDGE <br /> VENTURES
+            </h1>
+            <p className="header-subtext">
+              We structure and execute real estate development projects in a
+              responsible and rewarding manner based on careful asset selection,
+              optimal value addition, innovative financing structures and
+              targeted marketing and sales strategies.
+            </p>
+          </div>
         </div>
       </div>
     </div>
