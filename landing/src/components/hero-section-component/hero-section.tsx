bbeee1ee819553374bbd013b7f2cce7e6ea2e32d
ramesh-madara <rameshmadara@gmail.com>
2024-11-08 13:21:41 +0530

    hero section 1 desktop

---
+++ landing/src/components/hero-section-component/hero-section.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./hero-section.modules.css";
+import headerImage from "../../assets/images/header_img.png";
+import logo from "../../assets/images/logo.svg";
+
+interface HeroSectionProps {}
+
+const HeroSection: React.FC<HeroSectionProps> = () => {
+  return (
+    <div className="header-outer">
+      <div className="header-image"></div>
+      <div className="header-content">
+        <div className="header-logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="header-title-outer">
+          <h1 className="header-title">
+            CAMBRIDGE <br /> VENTURES
+          </h1>
+          <p className="header-subtext">
+            We structure and execute real estate development projects in a
+            responsible and rewarding manner based on careful asset selection,
+            optimal value addition, innovative financing structures and targeted
+            marketing and sales strategies.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
