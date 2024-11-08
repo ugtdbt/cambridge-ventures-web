ecd98d4e9dde057ed8e6cd945e18136e4df0e39c
ramesh-madara <rameshmadara@gmail.com>
2024-11-08 16:10:29 +0530

    out values section desktop tab mobile

--- landing/src/components/hero-section-component/hero-section.tsx
+++ landing/src/components/hero-section-component/hero-section.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./hero-section.modules.css";
-import headerImage from "../../assets/images/header_img.png";
 import logo from "../../assets/images/logo_cv.png";
+import our_values_image from "../../assets/images/our-values.png";
 
 interface HeroSectionProps {}
 
 const HeroSection: React.FC<HeroSectionProps> = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // Track scroll position
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    // Add scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div>
+    <div className="hero-section-outer">
       <div className="header-outer">
         <div className="header-image"></div>
         <div className="header-content">
@@ -23,6 +43,45 @@ const HeroSection: React.FC<HeroSectionProps> = () => {
               responsible and rewarding manner based on careful asset selection,
               optimal value addition, innovative financing structures and
               targeted marketing and sales strategies.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="our-value-outer">
+        <div className="ov-image-outer">
+          <img
+            className={`ov-image ${isScrolled ? "scrolled" : ""}`}
+            src={our_values_image}
+            alt=""
+          />
+        </div>
+        <div className="ov-content">
+          <h4 className="ov-content-header">
+            Our Value-Addition Activities <br />
+            Include The Following
+          </h4>
+          <div className="ov-content-item">
+            <h6 className="ov-content-item-topic">Investment</h6>
+            <p>
+              We invest in lands which we may either hold for a period of time
+              or engage in property developments projects such as Gated
+              Communities other Residential Projects
+            </p>
+          </div>
+          <div className="ov-content-item">
+            <h6 className="ov-content-item-topic">Joint Ventures</h6>
+            <p>
+              Landowners who are interested in developing their properties are
+              invited to contact us to explore potential collaborations
+            </p>
+          </div>
+          <div className="ov-content-item">
+            <h6 className="ov-content-item-topic">Structuring and Financing</h6>
+            <p>
+              Create and structure investments in the Real Estate industry{" "}
+              <span className="invisible">
+                sdfjhsdjkfhaskdjfhasdkjfhaskfhkasdhfklasfhkasdjfh
+              </span>
             </p>
           </div>
         </div>
